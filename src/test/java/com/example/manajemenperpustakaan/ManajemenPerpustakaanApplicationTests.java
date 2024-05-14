e07fd478f50647978ba5ce4cf4f006aa180be490
ameliadewi19 <ameliadewi1929@gmail.com>
2024-05-14 16:53:41 +0700

    first commit

---
+++ src/test/java/com/example/manajemenperpustakaan/ManajemenPerpustakaanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manajemenperpustakaan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManajemenPerpustakaanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
